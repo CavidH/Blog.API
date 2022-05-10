09d99f3ff1ab9a0a602e61c02d3ca0139f8a9b7c
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-05-10 13:45:31 +0400

    DAL

---
+++ Blog.DATA/DAL/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Blog.CORE.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.DATA.DAL
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<PostCategory> PostCategories { get; set; }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Gender> Gender { get; set; }
+
+
+    }
+}
